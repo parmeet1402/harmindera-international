c24e1ef0c1b27b808bea0f50195551b0750ab758
Parmeet Singh <parmeet.asi@gmail.com>
2021-06-21 22:36:41 +0530

    add hide on scroll feature to navbar

--- src/components/layout/desktop/navbar/index.tsx
+++ src/components/layout/desktop/navbar/index.tsx
@@ -4,16 +4,26 @@ import { Logo } from "@content/assets/CustomIcon";
 import ExpandableSearchField from "@components/form/ExpandableSearchField";
 import Links from "./links";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Slide from "@material-ui/core/Slide";
 import { useTheme } from "@material-ui/core/styles";
 import MenuRoundedIcon from "@material-ui/icons/MenuRounded";
 import CloseRoundedIcon from "@material-ui/icons/CloseRounded";
 import { IconButton } from "@components/form/Button";
 import useUIStore from "@zustand/ui";
 
-const Navbar = () => {
+interface Props {
+  window?: () => Window;
+}
+
+const Navbar = (props: Props) => {
+  const { window } = props;
+
   const theme = useTheme();
   const { isMobileNavbarExpanded, expandMobileNavbar, shrinkMobileNavbar } = useUIStore();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
 
   const handleIconButtonClick = () => {
     if (isMobileNavbarExpanded) {
@@ -26,34 +36,36 @@ const Navbar = () => {
   // position="static"
 
   return (
-    <AppBar position="fixed" color="secondary">
-      <Toolbar>
-        <Logo />
-        {!isMobile ? (
-          <>
-            <Links />
-            <ExpandableSearchField />
-          </>
-        ) : (
-          <>
-            <IconButton
-              style={{ marginLeft: "auto" }}
-              onClick={handleIconButtonClick}
-              colorOverrides={{
-                icon: {
-                  color: "text",
-                  colorVariant: "primary",
-                  hover: "text",
-                  hoverVariant: "primary",
-                },
-              }}
-            >
-              {isMobileNavbarExpanded ? <CloseRoundedIcon /> : <MenuRoundedIcon />}
-            </IconButton>
-          </>
-        )}
-      </Toolbar>
-    </AppBar>
+    <Slide appear={false} direction="down" in={!trigger}>
+      <AppBar position="fixed" color="secondary">
+        <Toolbar>
+          <Logo />
+          {!isMobile ? (
+            <>
+              <Links />
+              <ExpandableSearchField />
+            </>
+          ) : (
+            <>
+              <IconButton
+                style={{ marginLeft: "auto" }}
+                onClick={handleIconButtonClick}
+                colorOverrides={{
+                  icon: {
+                    color: "text",
+                    colorVariant: "primary",
+                    hover: "text",
+                    hoverVariant: "primary",
+                  },
+                }}
+              >
+                {isMobileNavbarExpanded ? <CloseRoundedIcon /> : <MenuRoundedIcon />}
+              </IconButton>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Slide>
   );
 };
 
