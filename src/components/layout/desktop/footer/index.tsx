2269489bae8e8b20ff84eefb7dafe39709f3e7d3
Parmeet Singh <parmeet.asi@gmail.com>
2021-06-27 18:52:20 +0530

    add lower footer

--- src/components/layout/desktop/footer/index.tsx
+++ src/components/layout/desktop/footer/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Link from "@components/navigation/Link";
@@ -128,6 +129,7 @@ const useStyles = makeStyles(theme => ({
   gridRoot: {
     margin: "0 auto",
     width: "calc(100% - 60px)",
+    // justifyContent:"space"
   },
   linkStyles: {
     fontWeight: theme.typography.fontWeightBold,
@@ -183,69 +185,92 @@ const Footer = () => {
   } = useStyles();
   return (
     <footer className={footerStyles}>
-      <Container classes={{ root }}>
-        <Grid classes={{ root: gridRoot }} container justify="center" spacing={4}>
-          <Grid item classes={{ root: firstContainerStyles }}>
-            <Grid container direction="column">
-              <Grid classes={{ root: iconContainerStyles }}>
-                <Logo variant="light" size="large" />
-                <Grid classes={{ root: iconGridStyles }}>
-                  <IconButton
-                    colorOverrides={{
-                      icon: {
-                        color: "secondary",
-                        colorVariant: "main",
-                        hover: "secondary",
-                        hoverVariant: "main",
-                      },
-                    }}
-                  >
-                    <WhatsApp />
-                  </IconButton>
-                  <IconButton
-                    colorOverrides={{
-                      icon: {
-                        color: "secondary",
-                        colorVariant: "main",
-                        hover: "secondary",
-                        hoverVariant: "main",
-                      },
-                    }}
-                  >
-                    <RoomOutlined />
-                  </IconButton>
-                  <IconButton
-                    colorOverrides={{
-                      icon: {
-                        color: "secondary",
-                        colorVariant: "main",
-                        hover: "secondary",
-                        hoverVariant: "main",
-                      },
-                    }}
-                  >
-                    <PhoneOutlined />
-                  </IconButton>
-                  <IconButton
-                    colorOverrides={{
-                      icon: {
-                        color: "secondary",
-                        colorVariant: "main",
-                        hover: "secondary",
-                        hoverVariant: "main",
-                      },
-                    }}
-                  >
-                    <MailOutlineOutlined />
-                  </IconButton>
+      <Container>
+        <Container classes={{ root }}>
+          <Grid classes={{ root: gridRoot }} container justify="space-between" spacing={4}>
+            <Grid item classes={{ root: firstContainerStyles }}>
+              <Grid container direction="column">
+                <Grid classes={{ root: iconContainerStyles }}>
+                  <Logo variant="light" size="large" />
+                  <Grid classes={{ root: iconGridStyles }}>
+                    <IconButton
+                      colorOverrides={{
+                        icon: {
+                          color: "secondary",
+                          colorVariant: "main",
+                          hover: "secondary",
+                          hoverVariant: "main",
+                        },
+                      }}
+                    >
+                      <WhatsApp />
+                    </IconButton>
+                    <IconButton
+                      colorOverrides={{
+                        icon: {
+                          color: "secondary",
+                          colorVariant: "main",
+                          hover: "secondary",
+                          hoverVariant: "main",
+                        },
+                      }}
+                    >
+                      <RoomOutlined />
+                    </IconButton>
+                    <IconButton
+                      colorOverrides={{
+                        icon: {
+                          color: "secondary",
+                          colorVariant: "main",
+                          hover: "secondary",
+                          hoverVariant: "main",
+                        },
+                      }}
+                    >
+                      <PhoneOutlined />
+                    </IconButton>
+                    <IconButton
+                      colorOverrides={{
+                        icon: {
+                          color: "secondary",
+                          colorVariant: "main",
+                          hover: "secondary",
+                          hoverVariant: "main",
+                        },
+                      }}
+                    >
+                      <MailOutlineOutlined />
+                    </IconButton>
+                  </Grid>
                 </Grid>
               </Grid>
             </Grid>
-          </Grid>
-          <Grid item classes={{ root: remainingContainerStyles }}>
-            <Grid direction="column" container>
-              <Typography variant="h5">Quick links</Typography>
-              {data.quickLinks.map(item => {
+            <Grid item classes={{ root: remainingContainerStyles }}>
+              <Grid direction="column" container>
+                <Typography variant="h5">Quick links</Typography>
+                {data.quickLinks.map(item => {
+                  return (
+                    <Grid>
+                      {item.icon ? item.icon : <></>}
+                      <Grid direction="column">
+                        {item.links.map(link => (
+                          <Link
+                            href={link.link}
+                            // style={{ marginLeft: "120px" }}
+                            classes={{ root: linkStyles }}
+                          >
+                            <Typography variant="body2">{link.label}</Typography>
+                          </Link>
+                        ))}
+                      </Grid>
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            </Grid>
+            <Grid item classes={{ root: remainingContainerStyles }}>
+              <Typography variant="h5">Services</Typography>
+              {data.services.map(item => {
                 return (
                   <Grid>
                     {item.icon ? item.icon : <></>}
@@ -253,8 +278,34 @@ const Footer = () => {
                       {item.links.map(link => (
                         <Link
                           href={link.link}
-                          style={{ marginLeft: "120px" }}
-                          classes={{ root: linkStyles }}
+                          // style={{ marginLeft: "120px" }}
+                          classes={{
+                            root: linkStyles,
+                          }}
+                        >
+                          <Typography variant="body2">{link.label}</Typography>
+                        </Link>
+                      ))}
+                    </Grid>
+                  </Grid>
+                );
+              })}
+            </Grid>
+            <Grid item classes={{ root: remainingContainerStyles }}>
+              <Typography variant="h5" style={{ marginBottom: "20px" }}>
+                Reach us
+              </Typography>
+              {data.reachUs.map(item => {
+                return (
+                  <Grid container direction="row" wrap="nowrap">
+                    <Link href={item.links[0].link}>{item.icon ? item.icon : <></>}</Link>
+                    <Grid container direction="column" style={{ marginBottom: "16px" }}>
+                      {item.links.map(link => (
+                        <Link
+                          href={link.link}
+                          classes={{
+                            root: linkStyles,
+                          }}
                         >
                           <Typography variant="body2">{link.label}</Typography>
                         </Link>
@@ -265,53 +316,28 @@ const Footer = () => {
               })}
             </Grid>
           </Grid>
-          <Grid item classes={{ root: remainingContainerStyles }}>
-            <Typography variant="h5">Services</Typography>
-            {data.services.map(item => {
-              return (
-                <Grid>
-                  {item.icon ? item.icon : <></>}
-                  <Grid direction="column">
-                    {item.links.map(link => (
-                      <Link
-                        href={link.link}
-                        style={{ marginLeft: "120px" }}
-                        classes={{
-                          root: linkStyles,
-                        }}
-                      >
-                        <Typography variant="body2">{link.label}</Typography>
-                      </Link>
-                    ))}
-                  </Grid>
-                </Grid>
-              );
-            })}
-          </Grid>
-          <Grid item classes={{ root: remainingContainerStyles }}>
-            <Typography variant="h5" style={{ marginBottom: "20px" }}>
-              Reach us
-            </Typography>
-            {data.reachUs.map(item => {
-              return (
-                <Grid container direction="row" wrap="nowrap">
-                  <Link href={item.links[0].link}>{item.icon ? item.icon : <></>}</Link>
-                  <Grid container direction="column" style={{ marginBottom: "16px" }}>
-                    {item.links.map(link => (
-                      <Link
-                        href={link.link}
-                        classes={{
-                          root: linkStyles,
-                        }}
-                      >
-                        <Typography variant="body2">{link.label}</Typography>
-                      </Link>
-                    ))}
-                  </Grid>
-                </Grid>
-              );
-            })}
-          </Grid>
+        </Container>
+        <Grid container direction="row" style={{ color: "white" }}>
+          <Typography
+            variant="subtitle2"
+            style={{ flexGrow: "1", marginLeft: "12ch" }}
+            color="secondary"
+          >
+            © 2021 Harmindera International. All rights reserved
+          </Typography>
+          <Box style={{ display: "flex", marginLeft: "-12ch" }}>
+            <Link href="/terms-and-conditions">
+              <Typography variant="subtitle2" color="secondary">
+                Terms & Conditions
+              </Typography>
+            </Link>
+            &nbsp; &#124; &nbsp;
+            <Link href="/privacy-policy">
+              <Typography variant="subtitle2" color="secondary" style={{ marginRight: "26ch" }}>
+                Privacy Policy
+              </Typography>
+            </Link>
+          </Box>
         </Grid>
       </Container>
     </footer>
