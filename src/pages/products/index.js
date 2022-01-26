14905f9e62e56f883e157e99d646da1ad2bc2c39
Parmeet Singh <parmeet.asi@gmail.com>
2022-01-27 02:50:35 +0530

    add dynamic category and product pages

---
+++ src/pages/products/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AllCategories = () => {
+  return (
+    <div>
+      <h2>AllCategories AllCategories</h2>
+      <h2>AllCategories AllCategories</h2>
+      <h2>AllCategories AllCategories</h2>
+    </div>
+  );
+};
+
+export default AllCategories;
---
+++ src/pages/products/{contentfulProductCategory.name}.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SingleProduct = () => {
+  return <div>Single Product Category</div>;
+};
+
+export default SingleProduct;
