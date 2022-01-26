5afcfbd5057f9ed0721dfeeee88d476a3456a1d1
Parmeet Singh <parmeet.asi@gmail.com>
2022-01-27 03:02:31 +0530

    source single product data to page

--- src/pages/products/{contentfulProduct.category__name}/{contentfulProduct.slug}/index.js
+++ src/pages/products/{contentfulProduct.category__name}/{contentfulProduct.slug}/index.js
@@ -1,7 +1,53 @@
 import React from "react";
+import { graphql } from "gatsby";
 
-const SingleProduct = () => {
+const SingleProduct = ({ data }) => {
+  console.log(JSON.stringify(data, null, 2));
+  // required 
   return <div>Single Product</div>;
 };
+
+// DONE: get single product data
+export const query = graphql`
+  query($id: String!) {
+    contentfulProduct(id: { eq: $id }) {
+      id
+      name
+      brand {
+        name
+      }
+      category {
+        name
+      }
+      finish {
+        name
+      }
+      sizes
+      slug
+      threadType {
+        name
+      }
+      material {
+        name
+      }
+      images {
+        original_url
+      }
+      headType {
+        name
+      }
+      packagingType {
+        name
+      }
+      quantityUnit {
+        unit
+      }
+      shape {
+        name
+      }
+      whole
+    }
+  }
+`;
 
 export default SingleProduct;
