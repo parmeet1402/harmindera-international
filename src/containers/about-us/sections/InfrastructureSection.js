5df2005cc7f88eebe5223c0823dfb9bba56c7c7b
Parmeet Singh <parmeet.asi@gmail.com>
2022-02-03 04:32:59 +0530

    Fix alignment and Add star icon to infrastructure section

--- src/containers/about-us/sections/InfrastructureSection.js
+++ src/containers/about-us/sections/InfrastructureSection.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Box, Container, Grid, makeStyles, Typography } from "@material-ui/core";
+import { Star as StarIcon } from "@material-ui/icons";
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -30,7 +31,7 @@ const useStyles = makeStyles(theme => ({
   },
   subHeading: {
     fontWeight: theme.typography.fontWeightBold,
-    fontSize: "0.7rem",
+    fontSize: "1.1rem",
     color: theme.palette.common.black,
     [theme.breakpoints.up("md")]: {
       fontSize: "0.78rem",
@@ -43,12 +44,13 @@ const useStyles = makeStyles(theme => ({
     },
   },
   listItemContainer: {
-    marginLeft: theme.spacing(0.5),
+    // marginLeft: theme.spacing(0.5),
     marginBlock: theme.spacing(0.8),
     color: theme.palette.grey["500"],
     fontSize: "0.5rem",
 
     [theme.breakpoints.up("md")]: {
+      fontSize: "0.5rem",
       marginInline: "auto",
       paddingInline: "0.5rem",
       marginBlock: 0,
@@ -56,6 +58,14 @@ const useStyles = makeStyles(theme => ({
     "@media only screen and (min-width: 1150px)": {
       //   paddingInline: "1rem",
     },
+  },
+  starIcon: {
+    fontSize: "1.6rem",
+    // color: "#f9cb00",
+    color: theme.palette.warning.main,
+    marginBottom: "-0.2rem",
+    marginLeft: "-0.1rem",
+    marginRight: "0.3rem",
   },
 }));
 
@@ -72,24 +82,26 @@ const InfrastructureSection = () => {
         <Grid container xs={11} md={4} className={classes.listItemContainer}>
           <Grid item xs={12}>
             <Typography variant="h6" className={classes.subHeading}>
+              <StarIcon className={classes.starIcon} />
               Modern and Well-Equipped Machinery
             </Typography>
           </Grid>
           <Grid item xs={12}>
-            <Typography variant="body2">
-              We at Harmindra International frequently keep investing into our manufacturing units to ensure
-              maximum quality and output in the shortest span of time.
+            <Typography variant="body1">
+              We at Harmindra International frequently keep investing into our manufacturing units
+              to ensure maximum quality and output in the shortest span of time.
             </Typography>
           </Grid>
         </Grid>
         <Grid container xs={11} md={4} className={classes.listItemContainer}>
           <Grid item xs={12}>
             <Typography variant="h6" className={classes.subHeading}>
+              <StarIcon className={classes.starIcon} />
               In House specialized manufacturing facilities
             </Typography>
           </Grid>
           <Grid item xs={12}>
-            <Typography variant="body2">
+            <Typography variant="body1">
               There's an in-house highly advanced and efficient CNC machinery unit. The firm also
               houses a top of the line forging workshop.
             </Typography>
@@ -98,11 +110,12 @@ const InfrastructureSection = () => {
         <Grid container xs={11} md={4} className={classes.listItemContainer}>
           <Grid item xs={12}>
             <Typography variant="h6" className={classes.subHeading}>
+              <StarIcon className={classes.starIcon} />
               Highly proficient professionals
             </Typography>
           </Grid>
           <Grid item xs={12}>
-            <Typography variant="body2">
+            <Typography variant="body1">
               Highly proficient professionals are there in our firm's manufacturing team who not
               only keep the production levels high but also make sure the quality is not compromised
               too.
