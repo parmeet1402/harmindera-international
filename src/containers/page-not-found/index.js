6ea6f758201ee7122dae08e64e2f874e7ca984b8
Parmeet Singh <parmeet.asi@gmail.com>
2022-02-04 00:53:48 +0530

    Add Cusotm 404 Page

--- src/containers/page-not-found/index.js
+++ src/containers/page-not-found/index.js
@@ -1,12 +1,37 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { Container, Typography, makeStyles } from "@material-ui/core";
+import { SolidButton } from "@components/form/Button";
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    textAlign: "center",
+    marginTop: theme.spacing(10),
+  },
+  button: {
+    marginTop: theme.spacing(12),
+  },
+  heading: {
+    fontSize: "8rem",
+    marginBottom: 0,
+    [theme.breakpoints.up("lg")]: {
+      fontSize: "12rem",
+      marginBottom: "-1rem",
+    },
+  },
+}));
 
 const PageNotFoundContainer = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <Typography variant="h1">404</Typography>
+    <Container className={classes.container}>
+      <Typography variant="h1" className={classes.heading}>
+        404
+      </Typography>
       <Typography variant="h4">Page not found</Typography>
-    </div>
+      <SolidButton className={classes.button} href="/">
+        Go Home
+      </SolidButton>
+    </Container>
   );
 };
 
