e77d776e298fa069d09b38f922cf03637ef7b47e
Parmeet Singh <parmeet.asi@gmail.com>
2022-02-06 00:02:55 +0530

    add google search file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Layout, { SEO } from "@components/layout";
 import LandingContainer from "@containers/landing";
+import { Helmet } from "react-helmet";
 
 const LandingPage = (props: any) => {
   // console.log({ props });
   return (
     <Layout>
       <SEO />
+      <Helmet>
+        <meta
+          name="google-site-verification"
+          content="XGwn9uS-T99EQBc7lhJRjTTnV0C7fd8DHuv2j7fvKf0"
+        />
+      </Helmet>
       <LandingContainer />
     </Layout>
   );
